e842e9112576cf1a34ce83b9629ed96f70d89982
blakjagge <33291005+blakjagge@users.noreply.github.com>
2017-11-06 12:33:58 -0500

    Add files via upload

---
+++ CSE 1.3.10.py
@@ -0,0 +1,8 @@
+# Team Name: Blake Jaggers
+# Strategy Name: Betray 
+# Strategy Description: The AI will always betray. Realistically, the only outcomes for betraying are +100 and -250, as opposed to 0 and -500. The outcomes are better overall.
+# Move: A function that returns 'c' or 'b' 
+
+def move():
+    return 'b'
+    
